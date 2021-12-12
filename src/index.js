a6d1e48a66aea5cca1cd891ddd1b40bc3bcbdf48
Shoxruh <80831284+BlackDoppi@users.noreply.github.com>
2021-12-13 03:52:14 +0500

    Basic server file moved to src directory.

--- src/index.js
+++ src/index.js
@@ -21,7 +21,7 @@ app.get('/', async (req, res) => {
         }
     }
 
-    res.sendFile(path.join(__dirname, 'public', 'index.html'))
+    res.sendFile(path.join(__dirname, '../public', 'index.html'))
 })
 
 
