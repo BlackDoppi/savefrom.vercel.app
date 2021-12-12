26d68448618a9bc9fb98285ccc1b9e3fe5dd9148
Shoxruh <80831284+BlackDoppi@users.noreply.github.com>
2021-12-13 03:44:49 +0500

    Handled Enter key pressing

--- public/script.js
+++ public/script.js
@@ -3,27 +3,35 @@ const title = document.querySelector('#title')
 const thumbnail = document.querySelector('#thumbnail')
 const links = document.querySelector('#links')
 const result = document.querySelector('#result')
+const submit = document.querySelector('#submit')
 
 
-document.querySelector('#submit')
-    .addEventListener('click', async (ev) => {
-        ev.preventDefault()
-        links.innerHTML = ''
-        thumbnail.src = ''
-        title.innerHTML = "Please wait in a moment..."
+const main = async (event) => {
+    event.preventDefault()
+    links.innerHTML = ''
+    thumbnail.src = ''
+    title.innerHTML = "Please wait in a moment..."
 
-        try {
-            const response = await fetch("/?" + new URLSearchParams({ link }))
-            
-            const video = await response.json()
+    try {
+        const response = await fetch("/?" + new URLSearchParams({ link }))
 
-            thumbnail.src = video.videoDetails.thumbnails[0].url
-            title.innerText = video.videoDetails.title.slice(0, 70)
+        const video = await response.json()
 
-            for (let format of video.formats) {
-                links.innerHTML += `<a href="${format.url}">Download ${format.qualityLabel}</a> `
-            }
-        } catch (error) {
-            console.error(error);
+        thumbnail.src = video.videoDetails.thumbnails[0].url
+        title.innerText = video.videoDetails.title.slice(0, 70)
+
+        for (let format of video.formats) {
+            links.innerHTML += `<a href="${format.url}">Download ${format.qualityLabel}</a> `
         }
-    })
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+submit.addEventListener('keydown', (event) => {
+    if (event.keyCode == 13)
+        main(event)
+})
+
+submit.addEventListener('submit', main)
+submit.addEventListener('click', main)
