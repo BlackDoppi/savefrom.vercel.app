32cb2aeeb8db572318d1f53f44f92818b4b6fc22
Shoxruh <80831284+BlackDoppi@users.noreply.github.com>
2021-12-13 20:15:18 +0500

    All fixed

--- public/script.js
+++ public/script.js
@@ -1,13 +1,14 @@
 const title = document.querySelector('#title')
 const thumbnail = document.querySelector('#thumbnail')
-const links = document.querySelector('#links')
+const linksWrapper = document.querySelector('#links')
 const result = document.querySelector('#result')
 const submit = document.querySelector('#submit')
 
 
-const main = async (event) => {
-    event.preventDefault()
-    links.innerHTML = ''
+// Getting video link
+const main = async (ev) => {
+    ev.preventDefault()
+    linksWrapper.innerHTML = ''
     thumbnail.src = ''
     title.innerHTML = "Please wait in a moment..."
     const link = document.querySelector('#link').value
@@ -20,22 +21,48 @@ const main = async (event) => {
         title.innerText = video.videoDetails.title.slice(0, 70)
 
         for (let format of video.formats) {
-            let link = '/api/download/?' + new URLSearchParams({ link: format.url })
+            let url = format.url
             let { title } = video.videoDetails
-            let quality = format.qualityLabel
-            let a = `<a class="download" download="${title}" href="${link}">Download ${quality}</a>`
-            links.innerHTML += a
+            let qty = format.qualityLabel
+            let a = `<a class="download" download="${title}" href="${url}">Download ${qty}</a>`
+            linksWrapper.innerHTML += a
         }
+
+        const links = document.querySelectorAll('.download')
+
+        links.forEach(link => link.addEventListener('click', download))
     } catch (error) {
         console.error(error);
     }
 }
 
-submit.addEventListener('keydown', (event) => {
-    if (event.keyCode == 13)
-        main(event)
-})
 
-
+// Events for getting link
+submit.addEventListener('keydown', ev => { if (ev.keyCode == 13) main(ev) })
 submit.addEventListener('submit', main)
 submit.addEventListener('click', main)
+
+
+// Downlad video
+function onStartedDownload(id) {
+    console.log(`Started downloading: ${id}`);
+}
+
+
+function onFailed(error) {
+    console.log(`Download failed: ${error}`);
+}
+
+
+function download(ev) {
+    ev.preventDefault()
+    const downloadUrl = ev.target.href
+
+    const downloading = browser.downloads.download({
+        url: downloadUrl,
+        filename: 'videoplayback.mp4',
+        conflictAction: 'uniquify'
+    });
+
+    downloading.then(onStartedDownload, onFailed);
+}
